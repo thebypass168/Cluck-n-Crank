24c44c33b74c83b5e3bfa5fb12e360740c57a914
thebypass168 <main@thebypass168.com>
2026-05-13 10:59:41 -0400

    Add app.js stub for Cluck N CrAdd app.js stubank application
    Initial stub for Cluck N Crank app with versioning.

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// Cluck N Crank app stub
+// TODO: move inline JavaScript logic from index.html into this file.
+
+window.CNC = window.CNC || {};
+window.CNC.version = '0.0.1';
+
+// Basic guard so external script is loaded without errors.
+console.debug('Cluck N Crank app.js loaded');
